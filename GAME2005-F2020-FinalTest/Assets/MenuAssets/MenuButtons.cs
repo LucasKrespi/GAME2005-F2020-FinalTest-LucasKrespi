31182ca3ee313a224b75e849c73969c3d2cb4c37
lucaskrespi <lucas.krespi@gmail.com>
2020-12-16 10:10:36 -0500

    Menu scene created

---
+++ GAME2005-F2020-FinalTest/Assets/MenuAssets/MenuButtons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    //Play Button
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
